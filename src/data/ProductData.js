de6dfb403d1687840a086048b60cdc256124faa3
TheAlgorithm476 <thealgorithm476@gmail.com>
2024-03-20 10:39:27 +0100

    Add ProductData Jira Issue: SDP2324A02-161

---
+++ src/data/ProductData.js
@@ -0,0 +1,61 @@
+import { prisma } from "./DatabaseAccessor";
+import { getLogger } from "../core/logging";
+
+/**
+ * @param product { null | { ProductId: number, Price: number, Stock: number, UnitOfMeasurement: string, ProductCategory: number, ProductAvailability: string, CreatedAt: Date, UpdatedAt: Date, IsActive: boolean } }
+ * @returns { undefined | { unit_of_measurement: string, updated_at: Date, price: number, product_id: number, product_availability: string, created_at: Date, active: boolean, stock: number, product_category: number } }
+ */
+const mapPrismaType = (product) => (
+    product === null ? undefined : {
+        product_id: product.ProductId,
+        price: product.Price,
+        stock: product.Stock,
+        unit_of_measurement: product.UnitOfMeasurement,
+        product_category: product.ProductCategory,
+        product_availability: product.ProductAvailability,
+        created_at: product.CreatedAt,
+        updated_at: product.UpdatedAt,
+        active: product.IsActive
+    }
+);
+
+/**
+ * @returns {Promise<({unit_of_measurement: string, updated_at: Date, price: number, product_id: number, product_availability: string, created_at: Date, active: boolean, stock: number, product_category: number}|undefined)[]>}
+ */
+export const getAllProducts = async () => {
+    const products = await prisma.products.findMany();
+    return products.map((product) => mapPrismaType(product));
+};
+
+/**
+ *
+ * @param id {number}
+ * @returns {Promise<{unit_of_measurement: string, updated_at: Date, price: number, product_id: number, product_availability: string, created_at: Date, active: boolean, stock: number, product_category: number}|undefined>}
+ */
+export const getProductById = async (id) => {
+    const product = await prisma.products.findFirst({
+        where: { ProductId: id }
+    });
+
+    return mapPrismaType(product);
+};
+
+/**
+ *
+ * @param id {number}
+ * @returns {Promise<boolean>}
+ */
+export const deleteProduct = async (id) => {
+    const product = await getProductById(id);
+
+    if (product === undefined) {
+        getLogger().warning(`Tried deleting nonexistent product ${id}`);
+        return false;
+    }
+
+    await prisma.products.delete({
+        where: { ProductId: id }
+    });
+
+    return true;
+};
