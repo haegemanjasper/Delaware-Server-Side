3cca00104d1d47edbff3d992148d70de4abf2f98
TheAlgorithm476 <thealgorithm476@gmail.com>
2024-03-21 09:29:20 +0100

    Add NotificationData Jira Issue: SDP2324A02-154

---
+++ src/data/NotificationData.js
@@ -0,0 +1,67 @@
+const { prisma } = require("./DatabaseAccessor");
+const { getLogger } = require("../core/logging");
+
+/**
+ * @param notification { null | { NotificationId: number, Date: Date, Text: string, Status: string, Username: string, CreatedAt: Date, UpdatedAt: Date, IsActive: boolean } }
+ * @returns { undefined | { date: Date, updated_at: Date, created_at: Date, active: boolean, notification_id: number, text: string, status: string, username: string } }
+ */
+const mapPrismaType = (notification) => (
+    notification === null ? undefined : {
+        notification_id: notification.NotificationId,
+        date: notification.Date,
+        text: notification.Text,
+        status: notification.Status,
+        username: notification.Username,
+        created_at: notification.CreatedAt,
+        updated_at: notification.UpdatedAt,
+        active: notification.IsActive
+    }
+);
+
+/**
+ *
+ * @returns {Promise<({date: Date, updated_at: Date, created_at: Date, active: boolean, notification_id: number, text: string, status: string, username: string}|undefined)[]>}
+ */
+const getAllNotifications = async () => {
+    const notifications = await prisma.notifications.findMany();
+    return notifications.map((notification) => mapPrismaType(notification));
+};
+
+/**
+ *
+ * @param id{number}
+ * @returns {Promise<{date: Date, updated_at: Date, created_at: Date, active: boolean, notification_id: number, text: string, status: string, username: string}|undefined>}
+ */
+const getNotificationById = async (id) => {
+    const notification = await prisma.notifications.findFirst({
+        where: { NotificationId: id }
+    });
+
+    return mapPrismaType(notification);
+};
+
+/**
+ *
+ * @param id{number}
+ * @returns {Promise<boolean>}
+ */
+const deleteNotification = async (id) => {
+    const notification = getNotificationById(id);
+
+    if (notification === undefined) {
+        getLogger().warning(`Tried deleting nonexistent notification ${id}`);
+        return false;
+    }
+
+    await prisma.notifications.delete({
+        where: { NotificationId: id }
+    });
+
+    return true;
+};
+
+module.exports = {
+    getAllNotifications,
+    getNotificationById,
+    deleteNotification
+};
